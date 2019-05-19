ac0a082072489677f6d4df5fcec26bbf81931956
dektox <dektox@users.noreply.github.com>
2019-05-20 01:13:44 +0300

    Delete server.js

--- server.js
+++
@@ -1,9 +0,0 @@
-var http = require('http');
-var static = require('node-static');
-var file = new static.Server('.');
-
-http.createServer(function(req, res) {
-  file.serve(req, res);
-}).listen(7077);
-
-console.log('Server running on port 7077');
