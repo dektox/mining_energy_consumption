cdedb141bc3724ad8209213babb8ded50e94771d
dektox <dektox@users.noreply.github.com>
2019-06-14 13:37:49 +0300

    Update comparisons.vue

--- frontend/pages/comparisons.vue
+++ frontend/pages/comparisons.vue
@@ -7,7 +7,7 @@
             <v-flex>
                 <v-layout align-center justify-center>
                     <v-flex xs10>
-                        <span style="font-size: 16px">While Terawatt-hours (TWh) are a standard unit of energy used to measure energy production and consumption, it can be difficult for laymen to assess without additional context.
+                        <span style="font-size: 16px"><br />While Terawatt-hours (TWh) are a standard unit of energy used to measure energy production and consumption, it can be difficult for laymen to assess without additional context.
 We provide a set of comparisons below to help readers put numbers into perspective.
 All comparisons are based on our best estimate of Bitcoin's total energy consumption
             </span>
