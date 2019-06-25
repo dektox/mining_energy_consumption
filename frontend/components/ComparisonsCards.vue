a13f1cb8219036bbd12a1e1832e5e6508beb9d61
dektox <dektox@users.noreply.github.com>
2019-06-25 16:39:56 +0300

    Update ComparisonsCards.vue

--- frontend/components/ComparisonsCards.vue
+++ frontend/components/ComparisonsCards.vue
@@ -2,7 +2,7 @@
     <v-flex my-4>
         <v-layout align-center justify-center my-4>
             <h2 class="display-1">
-                Country Ranking
+                <n/>Country Ranking
             </h2>
         </v-layout>
         <v-layout v-bind="binding" align-center justify-center>
