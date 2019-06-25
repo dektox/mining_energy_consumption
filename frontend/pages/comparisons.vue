09f1d317b9b1553282098374eeebfe0e552df8df
dektox <dektox@users.noreply.github.com>
2019-06-25 19:50:38 +0300

    Update comparisons.vue

--- frontend/pages/comparisons.vue
+++ frontend/pages/comparisons.vue
@@ -5,8 +5,11 @@
                 <h1 class="display-2">Comparisons</h1>
             </v-layout>
             <p class="main-text">While Terawatt-hours (TWh) are a standard unit of energy used to measure energy production and consumption, it can be difficult for laymen to assess without additional context.
-We provide a set of comparisons below to help readers put numbers into perspective.
-All comparisons are based on our best estimate of Bitcoin's total energy consumption</p>
+We provide a set of comparisons below to help readers put numbers into perspective.</p>
+            <p class="main-text"></p>
+            <p class="main-text">This section attempts to provide an unbiased and objective ground for helping visitors to independently assess the magnitude of Bitcoin’s electricity consumption and compare it to other uses of electricity.</p>
+            <p class="main-text"></p>
+            <p class="main-text"><em><strong>Note:<strong/> All comparisons are based on our best-guess estimate of Bitcoin's total electricity consumption.</em></p>
             <comparisonsPC />
             <comparisonsRP />
             <comparisonsCards />
