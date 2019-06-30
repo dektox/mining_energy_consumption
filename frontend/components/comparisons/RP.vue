11b2e75b3c7bab74053b1e2b89f9bf1c3dc84ffe
dektox <dektox@users.noreply.github.com>
2019-06-30 16:22:17 +0300

    Update RP.vue

--- frontend/components/comparisons/RP.vue
+++ frontend/components/comparisons/RP.vue
@@ -82,7 +82,7 @@
         <v-layout my-3>
           <span>
               <b>Source:</b><br/>
-              International Energy Agency <a href="https://www.iea.org/statistics/electricity/" target="_blank">report</a>, 2016 est.
+              <a href="https://www.iea.org/statistics/electricity/">International Energy Agency</a>, 2016 est.
           </span>
         </v-layout>
     </v-flex>
