dd531467c81ba0283d67dd8f257bb70ed4123673
chesanovskiy_dv <chesanovskiydv@gmail.com>
2021-02-16 15:20:41 +0200

    fix countries api

--- api/chart_API.py
+++ api/chart_API.py
@@ -270,7 +270,7 @@ def recalculate_guess(value):
 
 @app.route("/api/countries")
 def countries_btc():
-    tup2dict = {a:[c,d] for a,b,c,d in countries}
+    tup2dict = {a:[c,d] for a,b,c,d,e,f in countries}
     tup2dict['Bitcoin'][0] = round(cons[-1][4],2)
     dictsort = sorted(tup2dict.items(), key = lambda i: i[1][0], reverse=True)
     response = []
